1a10c086676a9242466aa32a0c6fba060b036a96
joyboy18900 <surakiat.dev@gmail.com>
2023-11-06 22:55:47 +0700

    init a project liltle banking

---
+++ src/main/java/com/example/bankingapplicationsmalltodo/BankingApplicationSmallTodoApplication.java
@@ -0,0 +1,13 @@
+package com.example.bankingapplicationsmalltodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingApplicationSmallTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingApplicationSmallTodoApplication.class, args);
+	}
+
+}
