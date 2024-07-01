0a6597dfeb67c6e5f93d9fd41407143da5feade0
joyboy18900 <surakiat.dev@gmail.com>
2024-07-01 23:31:45 +0700

    Refactor route to used ResponseEntity for response

--- src/main/java/com/example/bankingapplicationsmalltodo/account/AccountController.java
+++ src/main/java/com/example/bankingapplicationsmalltodo/account/AccountController.java
@@ -1,8 +1,12 @@
 package com.example.bankingapplicationsmalltodo.account;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -13,24 +17,64 @@ public class AccountController {
     private AccountService accountService;
 
     @PostMapping
-    public Account createAccount(@RequestBody Account account) {
-        return accountService.createAccount(account);
+    public ResponseEntity<AccountResponse> createAccount(@RequestBody AccountRequest accountRequest) {
+        Account account = accountService.createAccount(accountRequest);
+
+        List<Account> accountList = new ArrayList<>();
+        accountList.add(new Account(account.getId(), account.getAccountHolderName(), account.getBalance()));
+
+        AccountResponse accountResponse = new AccountResponse();
+        accountResponse.setCode(String.valueOf(HttpStatus.CREATED.value()));
+        accountResponse.setAccountList(accountList);
+        accountResponse.setStatus(true);
+
+        return new ResponseEntity<>(accountResponse, HttpStatus.CREATED);
     }
 
     @GetMapping("/{id}")
-    public Account getAccount(@PathVariable Long id) {
-        return accountService.getAccount(id).orElseThrow(() -> new RuntimeException("Account not found"));
+    public ResponseEntity<AccountResponse> getAccount(@PathVariable Long id) {
+        Account account = accountService.getAccount(id).orElseThrow(() -> new RuntimeException("Account not found"));
+
+        List<Account> accountList = new ArrayList<>();
+        accountList.add(new Account(account.getId(), account.getAccountHolderName(), account.getBalance()));
+
+        AccountResponse accountResponse = new AccountResponse();
+        accountResponse.setCode(String.valueOf(HttpStatus.OK.value()));
+        accountResponse.setAccountList(accountList);
+        accountResponse.setStatus(true);
+
+        return new ResponseEntity<>(accountResponse, HttpStatus.OK);
     }
 
     @PostMapping("/{id}/deposit")
-    public Account deposit(@PathVariable Long id, @RequestBody Map<String, Double> request) {
+    public ResponseEntity<AccountResponse> deposit(@PathVariable Long id, @RequestBody Map<String, Double> request) {
         Double amount = request.get("amount");
-        return accountService.deposit(id, amount);
+        Account account = accountService.deposit(id, amount);
+
+        List<Account> accountList = new ArrayList<>();
+        accountList.add(new Account(account.getId(), account.getAccountHolderName(), account.getBalance()));
+
+        AccountResponse accountResponse = new AccountResponse();
+        accountResponse.setCode(String.valueOf(HttpStatus.OK.value()));
+        accountResponse.setAccountList(accountList);
+        accountResponse.setStatus(true);
+
+        return new ResponseEntity<>(accountResponse, HttpStatus.OK);
     }
 
     @PostMapping("/{id}/withdraw")
-    public Account withdraw(@PathVariable Long id, @RequestBody Map<String, Double> request) {
+    public ResponseEntity<AccountResponse> withdraw(@PathVariable Long id, @RequestBody Map<String, Double> request) {
         Double amount = request.get("amount");
-        return accountService.withdraw(id, amount);
+        Account account = accountService.withdraw(id, amount);
+
+        List<Account> accountList = new ArrayList<>();
+        accountList.add(new Account(account.getId(), account.getAccountHolderName(), account.getBalance()));
+
+        AccountResponse accountResponse = new AccountResponse();
+        accountResponse.setCode(String.valueOf(HttpStatus.OK.value()));
+        accountResponse.setAccountList(accountList);
+        accountResponse.setStatus(true);
+
+        return new ResponseEntity<>(accountResponse, HttpStatus.OK);
     }
 }
