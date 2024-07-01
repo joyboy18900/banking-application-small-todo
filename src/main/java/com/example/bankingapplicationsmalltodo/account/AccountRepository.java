26bfa23ebda404f9d8cfa388cd450d1ec509ef32
joyboy18900 <surakiat.dev@gmail.com>
2024-07-01 23:30:40 +0700

    Add function query find account data by holder name

--- src/main/java/com/example/bankingapplicationsmalltodo/account/AccountRepository.java
+++ src/main/java/com/example/bankingapplicationsmalltodo/account/AccountRepository.java
@@ -3,6 +3,9 @@ package com.example.bankingapplicationsmalltodo.account;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
+    List<Account> findByAccountHolderName(String accountHolderName);
 }
